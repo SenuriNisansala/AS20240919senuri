a200467c10e5eee91be6ab9f2ad847031641902f
SenuriNisansala <Senuri2net@gmail.com>
2025-10-25 18:14:01 +0530

    add and view city

--- JavaApplication12/src/javaapplication12/JavaApplication12.java
+++ JavaApplication12/src/javaapplication12/JavaApplication12.java
@@ -4,31 +4,61 @@
  */
 package javaapplication12;
 
-/**
- *
- * @author User
- */
-public class cityManagement {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+
+
+public class JavaApplication12 {
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<String> cities = new ArrayList<>();
     
 
- public static void main(String[] args) {
-     
-     
-     
-     
-  
- }
- static void addCity() {
-        if(cityCount >= MAX_CITIES) { System.out.println("City limit reached."); return; }
+    public static void main(String[] args) {
+        int choice;
+        do {
+            System.out.println("\n=== LOGISTICS MANAGEMENT SYSTEM ===");
+            System.out.println("1. Add Cities");
+            System.out.println("2. View Cities");
+            System.out.println("3. Add Delivery");
+            System.out.println("4. View All Deliveries");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            sc.nextLine(); 
+
+            switch (choice) {
+                case 1:
+                    addCity(); 
+                break;
+                case 2: 
+                    viewCities(); 
+                break;
+                default:
+                    System.out.println("Invalid choice! Try again.");
+            }
+        } while (choice != 5);
+    }
+    
+    //add a city
+    static void addCity() {
         System.out.print("Enter city name: ");
         String name = sc.nextLine();
-        for(int i=0;i<cityCount;i++)
-            if(cities[i].equalsIgnoreCase(name)) { System.out.println("City already exists."); return; }
-        cities[cityCount++] = name;
-        System.out.println("City added successfully.");
- }
-}
+        if (cities.contains(name)) {
+            System.out.println("City already exists!");
+        } else {
+            cities.add(name);
+            System.out.println("City added successfully!");
+        }
+    }
+    static void viewCities() {
+        if (cities.isEmpty()) {
+            System.out.println("No cities added yet.");
+            return;
+        }
+        System.out.println("\n--- List of Cities ---");
+        for (int i = 0; i < cities.size(); i++) {
+            System.out.println((i + 1) + ". " + cities.get(i));
+        }
+    }
+
